d9a438786d8e9ee18ba247f020a64fdbcbb02a12
Rhydonphilip <rhydonphilip@outlook.com>
2023-11-15 14:52:43 +0100

    Added user abilities and crit multipliers
    Make the base attack values from an array into an object for each mon with properties: base atk, crit, fainted. Perform the damage calc for all mons' corresponding hit unless fainted (boolean).
    
    Added a few existant boosting abilities for the user.
    
    Also fixed a bug with Terastralization giving a 2x boost even when not already recieving STAB.

--- BeatUpCalc/BeatUpJS.js
+++ BeatUpCalc/BeatUpJS.js
@@ -1,6 +1,6 @@
 //to-do list:
+//still duplicate calcs
 //doesn't have a chance based damage calculation
-//Make it so it doesn't show a duplicate of damage calcs
 
 let randoValues = [85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]; //all potential integers used for the randomized roll
 
@@ -11,7 +11,6 @@ document.getElementById("calcBtn").onclick = function() {
     let other = 1; //other multiplier used for practically everything ranging from weather to abilities to items
     document.getElementById("possOutPut").innerHTML = "Possible damage amounts: <br />";
     let resultString = document.getElementById("possOutPut").innerHTML; //output field for each individual damage roll
-    let baseAtkArray = [];
 
     //checking level
     let lvl = 5;
@@ -19,8 +18,8 @@ document.getElementById("calcBtn").onclick = function() {
     
     //checking STAB
     let stab = 1; //stab value
-    if (document.getElementById("stab").checked) stab = 1.5;
-    if (document.getElementById("tera").checked) stab = 2;
+    if (document.getElementById("stab").checked) {stab += 0.5;};
+    if (document.getElementById("tera").checked) {stab += 0.5;};
 
     //checking usr's attack value
     let usrAtkBstMult = staStagSwtch(+document.getElementById("attackStage").value);
@@ -62,70 +61,107 @@ document.getElementById("calcBtn").onclick = function() {
     let maxHP = +document.getElementById("oppMaxHP").value;
     if (currentHP > maxHP) currentHP = maxHP;
 
-    //checking for fainted party members, and addressing the respective attack values if existant
-    baseAtkArray.push(+document.getElementById("atkMon1").value);
-    for (let v = 2; v < 7; v++){
-        let hkBoxMonCount = "hkBoxMon" + v;
-        if (!document.getElementById(hkBoxMonCount).checked){
-            let selector = "atkMon" + v;
-            baseAtkArray.push(+document.getElementById(selector).value);
-            console.log(baseAtkArray[v-2]);
+    //make mon objects
+    let monObjects = [
+        mon1 = {
+            baseAtk: +document.getElementById("atkMon1").value,
+            fainted: 0,
+            crit: 1
+        },
+        mon2 = {
+            baseAtk: +document.getElementById("atkMon2").value,
+            fainted: 0,
+            crit: 1
+        },
+        mon3 = {
+            baseAtk: +document.getElementById("atkMon3").value,
+            fainted: 0,
+            crit: 1
+        },
+        mon4 = {
+            baseAtk: +document.getElementById("atkMon4").value,
+            fainted: 0,
+            crit: 1
+        },
+        mon5 = {
+            baseAtk: +document.getElementById("atkMon5").value,
+            fainted: 0,
+            crit: 1
+        },
+        mon6 = {
+            baseAtk: +document.getElementById("atkMon6").value,
+            fainted: 0,
+            crit: 1
         }
-    }    
+    ];
+
+    //check each mon for faint and crit multipliers
+    for (let p = 1; p <= 6; p++) {
+        let crBoxMon_X = "crBoxMon" + p;
+        if (document.getElementById(crBoxMon_X).checked)
+        {
+            monObjects[p-1].crit = 1.5;
+        }
+        let hkBoxMon_X = "hkBoxMon" + p;
+        if (p != 1) {
+            if (document.getElementById(hkBoxMon_X).checked){monObjects[p-1].fainted = 1;};
+        }
+    }
 
     //checking for Items and Burn
     if (document.getElementById("burn").checked) other *= 0.5;
     if (document.getElementById("oppItem").value == "Eviolite") other /= 1.5;
     if (document.getElementById("userItem").value == "Black Glasses") other *= 1.2;
     if (document.getElementById("userItem").value == "Life Orb") other *= 1.3;
+    if (document.getElementById("userAbility").value == "Hustle") other *= 1.5;
+    if (document.getElementById("userAbility").value == "Tough Claws") other *= (5325 /4096);
+    if (document.getElementById("userAbility").value == "Technician") {
+        let base = (monObjects[1].baseAtk / 10) + 5;
+        base = Math.floor(base);
+        if (base <= 60) {other *= 1.5;}; //research implies user's atk is solely checked to then boost all following (akin to other multihits)
+    }
+    //let technicianBool = false;
     if (document.getElementById("userItem").value == "Choice Band") {
         let tempAtk = usrAtk * 1.5;
         Math.floor(tempAtk); //potential issue
         usrAtk = tempAtk;
     } 
 
-    //check for reflect, sr, spikes, status conditions
-    if (document.getElementById("reflect").checked) {
-        other = other / 2;
-    }
+    //checks for reflect
+    if (document.getElementById("reflect").checked) {other /= 2;};
+    //Checks for sr, spikes, status conditions
     residDmgCheck();
 
-
+    //unfinished multiline, this was to have a fractional %
+    /*
     document.getElementById("outputP").innerHTML = "Beat Up has a " + (randKOCount / 16) + "% chance to " ;
     //need to run through all the rando values, and make it output each instance it did KO
     if (damageCalc(randoValues[0]) <= 0){
-        document.getElementById("possOutPut").innerHTML = resultString;
         document.getElementById("outputP").innerHTML += "OHKO";
     } else if (damageCalc(randoValues[0]) <= (maxHP/2)) {
-        document.getElementById("possOutPut").innerHTML = resultString;
         document.getElementById("outputP").innerHTML += "2HKO";
     } else {
         turnCnt = 2;
         residDmgCheck();
         if (damageCalc(randoValues[0]) <= (maxHP/2)){
-        document.getElementById("possOutPut").innerHTML = resultString;
         document.getElementById("outputP").innerHTML += "2HKO after taking double hazards";
-        } else {
-        document.getElementById("possOutPut").innerHTML = resultString;
         }
-    }
+    }*/
 
-    //bit future proofed to see how many hits would be needed to get a KO, mid-move.
+    //performs the calc for each damage roll in our roll array
     for (let j = 0; j < randoValues.length; j++){
-        if (damageCalc(randoValues[j]) <= 0){
-            randKOCount++;
-        }
+        if (damageCalc(randoValues[j]) <= 0){randKOCount++;};
     }    
 
     //possible damage roll's as an output; need to be turned back into HTML
     document.getElementById("possOutPut").innerHTML = resultString;
 
-    if (damageCalc(randoValues[0]) <= 0) document.getElementById("outputP").innerHTML = "It always KO's"; //if the min roll always KO's
-    
+    if (damageCalc(randoValues[0]) <= 0) {document.getElementById("outputP").innerHTML = "It always KO's";}; //if the min roll always KO's
+
 
     //internal Functions
     function residDmgCheck(){
-        //checks for Stealth Rocks and Spikes
+        //checks for Stealth Rocks
         if (document.getElementById("sr").checked) {
             let divSR = 8;
             if (type1 == "Bug" || type1 == "Flying" || type1 == "Ice" || type1 == "Fire") {
@@ -179,8 +215,8 @@ document.getElementById("calcBtn").onclick = function() {
 
     //switch to determine fractions for stat stages
     function staStagSwtch(x){
-        //this changes what is the stat stage value ranging from -6 to 6 into a boost multiplier using its fraction. 
-        //If negative the fraction is changed on the bottom. If possitive it instead increases on the top. it's 2/2 by default aka a 1x multiplier
+        //this changes what the stat stage value is, ranging from -6 to 6 into a boost multiplier using its fraction. 
+        //If negative: the fraction is changed on the bottom. If possitive it instead increases on the top. It's 2/2 by default aka a 1x multiplier
         let top = 2;
         let bott = 2;
         switch (x) {
@@ -237,85 +273,94 @@ document.getElementById("calcBtn").onclick = function() {
         //initializing the defense stage and adjusting it only as a variable in here
         let oppDefStage = +document.getElementById("defStage").value;
         let resHP = currentHP;
-        let basePow, damage;
         //the damage calc and damage being removed from total HP
-        for (let i = 0; i < baseAtkArray.length; i++) {
-            //Adjusting potential stat states that may change mid attack (Weak Armor or Stamina)
-            oppDefMult = staStagSwtch(oppDefStage);
-            if (document.getElementById("oppAbility").value == "Fur Coat"){
-                oppDef = (+document.getElementById("oppDefense").value) * 2 * oppDefMult;
-            } else {
-                oppDef = (+document.getElementById("oppDefense").value) * oppDefMult;
-            }
-
-            //the actual damage determination:
-            basePow = (baseAtkArray[i] / 10) + 5;
-            basePow = Math.floor(basePow);
-    
-            damage = ((2*lvl)/5 + 2) * basePow;
-            damage = Math.floor(damage);
-            damage = (damage * usrAtk) / oppDef;
-            /* //it does not appear calcing it in the above written way or in this commented out way, would change the resulting damage
-            damage *= usrAtk;
-            damage = Math.floor(damage);
-            damage /= oppDef; 
-            */
-            damage = Math.floor(damage);
-            // if (damage == 0) damage = 1;
-            damage /= 50;
-            damage = Math.floor(damage);
-            // if (damage == 0) damage = 1;
-            damage += 2;
-            damage = Math.floor(damage);
-            damage *= rando;
-            damage = Math.floor(damage);
-            damage *= stab;
-            damage = Math.floor(damage);
-            damage *= typeRes;
-            damage = Math.floor(damage);
-            damage *= other;
-            damage = Math.floor(damage);
-            if (damage == 0) damage = 1; //end of the damage determination
-
-            //appending a seperator comma for our list of individual hits
-            resultString += damage + ", "; 
-    
-            //stamina and Weak armor procs
-            if (document.getElementById("oppAbility").value == "Weak Armor gen5-6"){
-                oppDefStage--;
-                if (oppDefStage <= -6) oppDefStage = -6;
-            }
-    
-            if (document.getElementById("oppAbility").value == "Weak Armor gen7+"){
-                oppDefStage -= 2;
-                if (oppDefStage <= -6) oppDefStage = -6;
-            }
-    
-            if (document.getElementById("oppAbility").value == "Stamina"){
-                oppDefStage++;
-                if (oppDefStage >= 6) oppDefStage = 6;
-            }
-    
-            //reducing HP from total
-            resHP -= damage;
-            if (document.getElementById("oppItem").value == "Berry Juice") {
-                if ((maxHP / 2) > resHP) {
-                    resHP += 20;
-                    if (resHP > maxHP) resHP = maxHP;
-                }
-            } else if (document.getElementById("oppItem").value == "Oran Berry") {
-                if ((maxHP / 2) > resHP) {
-                    resHP += 10;
-                    if (resHP > maxHP) resHP = maxHP;
-                }
-            }
-    
-            if (KO == 0){
-                if (resHP <= 0) {
-                    document.getElementById("outputP").innerHTML = "KO's"; // in " + (i+1) + " out of " + baseAtkArray.length + " potential hits"; //unfinished
-                    KO = 1;
+        for (let i = 0; i < 6; i++) {
+            if (monObjects[i].fainted == 0) 
+            {    
+                //Adjusting potential stat states that may change mid attack (Weak Armor or Stamina)
+                oppDefMult = staStagSwtch(oppDefStage);
+                if (document.getElementById("oppAbility").value == "Fur Coat"){
+                    oppDef = (+document.getElementById("oppDefense").value) * 2 * oppDefMult;
                 } else {
-                    document.getElementById("outputP").innerHTML = "remaining HP = " +  resHP + " out of " + maxHP;
+                    oppDef = (+document.getElementById("oppDefense").value) * oppDefMult;
+                }
+
+                //the true damage calc:
+                let basePow = (monObjects[i].baseAtk / 10) + 5;
+                basePow = Math.floor(basePow);
+                /*if (basePow <= 60) {
+                    if (technicianBool != true) {
+                        if (document.getElementById("userAbility").value == "Technician") {
+                            other *= 1.5;
+                            technicianBool = true;
+                        };
+                    }
+                }*/
+                let damage = ((2*lvl)/5 + 2) * basePow;
+                damage = Math.floor(damage);
+                damage *= usrAtk;
+                damage = Math.floor(damage);
+                damage /= oppDef; 
+                damage = Math.floor(damage);
+                // if (damage == 0) damage = 1;
+                damage /= 50;
+                damage = Math.floor(damage);
+                // if (damage == 0) damage = 1;
+                damage += 2; //beyond me why this exists in the original damage calc
+                damage = Math.floor(damage);
+                damage *= rando;
+                damage = Math.floor(damage);
+                damage *= stab;
+                damage = Math.floor(damage);
+                damage *= typeRes;
+                damage = Math.floor(damage);
+                damage *= other;
+                damage = Math.floor(damage);
+                damage *= monObjects[i].crit;
+                damage = Math.floor(damage);
+                if (damage == 0) damage = 1;
+                //end of the true damage calc
+
+                //appending a seperator comma for our list of individual hits
+                resultString += damage + ", "; 
+        
+                //stamina and Weak armor procs
+                if (document.getElementById("oppAbility").value == "Weak Armor gen5-6"){
+                    oppDefStage--;
+                    if (oppDefStage <= -6) oppDefStage = -6;
+                }
+        
+                if (document.getElementById("oppAbility").value == "Weak Armor gen7+"){
+                    oppDefStage -= 2;
+                    if (oppDefStage <= -6) oppDefStage = -6;
+                }
+        
+                if (document.getElementById("oppAbility").value == "Stamina"){
+                    oppDefStage++;
+                    if (oppDefStage >= 6) oppDefStage = 6;
+                }
+        
+                //reducing HP from total
+                resHP -= damage;
+                if (document.getElementById("oppItem").value == "Berry Juice") {
+                    if ((maxHP / 2) > resHP) {
+                        resHP += 20;
+                        if (resHP > maxHP) resHP = maxHP;
+                    }
+                } else if (document.getElementById("oppItem").value == "Oran Berry") {
+                    if ((maxHP / 2) > resHP) {
+                        resHP += 10;
+                        if (resHP > maxHP) resHP = maxHP;
+                    }
+                }
+        
+                if (KO == 0){
+                    if (resHP <= 0) {
+                        document.getElementById("outputP").innerHTML = "KO's";
+                        KO = 1;
+                    } else {
+                        document.getElementById("outputP").innerHTML = "remaining HP = " +  resHP + " out of " + maxHP;
+                    }
                 }
             }
         }
